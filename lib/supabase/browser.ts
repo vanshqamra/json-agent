4edd47e097b8c6aa268f6daacd9983d875135214
vanshqamra <VANSH.QAMRA@GMAIL.COM>
2025-11-09 02:44:18 +0530

    Create browser.ts

---
+++ lib/supabase/browser.ts
@@ -0,0 +1,8 @@
+// lib/supabase/browser.ts
+import { createBrowserClient } from '@supabase/ssr'
+
+export const createSupabaseBrowser = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
